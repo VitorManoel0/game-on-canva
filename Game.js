bce876037444c972da00d603e38759a8784a75c9
vitor manoel <vitor.silva@e-deploy.com.br>
2026-02-12 14:32:10 -0400

    test add virtual pad

--- Game.js
+++ Game.js
@@ -1,3 +1,52 @@
+function criandoGamePad() {
+    function simularTecla(codigo, tipo) {
+        const evento = new KeyboardEvent(tipo, {
+            key: codigo,
+            code: codigo === 'a' ? 'KeyA' : codigo === 'd' ? 'KeyD' : 'Space',
+            keyCode: codigo === 'a' ? 65 : codigo === 'd' ? 68 : 32,
+            which: codigo === 'a' ? 65 : codigo === 'd' ? 68 : 32,
+            bubbles: true
+        });
+        document.dispatchEvent(evento);
+    }
+
+    document.getElementById('btnEsquerda').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        simularTecla('a', 'keydown');
+    });
+    document.getElementById('btnEsquerda').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        simularTecla('a', 'keyup');
+    });
+
+    document.getElementById('btnDireita').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        simularTecla('d', 'keydown');
+    });
+    document.getElementById('btnDireita').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        simularTecla('d', 'keyup');
+    });
+
+    document.getElementById('btnPular').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        simularTecla(' ', 'keydown');
+    });
+    document.getElementById('btnPular').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        simularTecla(' ', 'keyup');
+    });
+
+    // Também funcionar com mouse (para testar no PC)
+    document.getElementById('btnEsquerda').addEventListener('mousedown', () => simularTecla('a', 'keydown'));
+    document.getElementById('btnEsquerda').addEventListener('mouseup', () => simularTecla('a', 'keyup'));
+
+    document.getElementById('btnDireita').addEventListener('mousedown', () => simularTecla('d', 'keydown'));
+    document.getElementById('btnDireita').addEventListener('mouseup', () => simularTecla('d', 'keyup'));
+
+    document.getElementById('btnPular').addEventListener('mousedown', () => simularTecla(' ', 'keydown'));
+    document.getElementById('btnPular').addEventListener('mouseup', () => simularTecla(' ', 'keyup'));
+}
 function carregaSons() {
     const somMoeda = new Audio('assets/brackeys_platformer_assets/sounds/coin.wav');
     somMoeda.volume = 0.1;
@@ -5,11 +54,11 @@ function carregaSons() {
     const somPulo = new Audio('assets/brackeys_platformer_assets/sounds/jump.wav');
     somPulo.volume = 0.1;
 
-    const somFase = new Audio('assets/background.mp3');
-    somFase.volume = 0.3;
+    // const somFase = new Audio('assets/background.mp3');
+    // somFase.volume = 0.3;
 
-    somFase.loop = true;
-    somFase.play();
+    // somFase.loop = true;
+    // somFase.play();
 
     return { somMoeda, somPulo };
 }
@@ -63,6 +112,7 @@ const mapa = new Mapa(ctx, 32, 32);
 let tileset = new Tileset("assets/brackeys_platformer_assets/sprites/world_tileset.png", 16, 16);
 let moedasColetadas = 0
 
+criandoGamePad()
 window.addEventListener('keydown', (e) => {
     const key = e.key;
     switch (key) {
